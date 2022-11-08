ea2478db4bb73198e6e430d8e572fed67d7fd1cf
Ivan Buetler <ivan.buetler@compass-security.com>
2022-11-08 20:13:24 +0100

    fix

--- build-release.sh
+++ build-release.sh
@@ -7,15 +7,15 @@ case `uname -m` in
 		echo "building x86_64"
 		docker buildx build --platform linux/amd64 --no-cache -t hackinglab/alpine-base-hl:amd64-latest -t hackinglab/alpine-base-hl:amd64-$1 -t hackinglab/alpine-base-hl:amd64-$1.0 -f Dockerfile.amd64 .
 
-		echo "=============================================="
-		echo "docker push hackinglab/alpine-base-hl:amd64-latest"
-		docker push hackinglab/alpine-base-hl:amd64-latest
-		echo "=============================================="
-		echo "docker push hackinglab/alpine-base-hl:amd64-$1"
-		docker push hackinglab/alpine-base-hl:amd64-$1
-		echo "=============================================="
-		echo "docker push hackinglab/alpine-base-hl:amd64-$1.0"
-		docker push hackinglab/alpine-base-hl:amd64-$1.0
+		#echo "=============================================="
+		#echo "docker push hackinglab/alpine-base-hl:amd64-latest"
+		#docker push hackinglab/alpine-base-hl:amd64-latest
+		#echo "=============================================="
+		#echo "docker push hackinglab/alpine-base-hl:amd64-$1"
+		#docker push hackinglab/alpine-base-hl:amd64-$1
+		#echo "=============================================="
+		#echo "docker push hackinglab/alpine-base-hl:amd64-$1.0"
+		#docker push hackinglab/alpine-base-hl:amd64-$1.0
 	;;
 
 	aarch64)
@@ -23,15 +23,15 @@ case `uname -m` in
                 echo "building aarch64"
                 docker buildx build --platform linux/arm64 --no-cache -t hackinglab/alpine-base-hl:arm64-latest -t hackinglab/alpine-base-hl:arm64-$1 -t hackinglab/alpine-base-hl:arm64-$1.0 -f Dockerfile.arm64 .
 
-                echo "=============================================="
-                echo "docker push hackinglab/alpine-base-hl:arm64-latest"
-                docker push hackinglab/alpine-base-hl:arm64-latest
-                echo "=============================================="
-                echo "docker push hackinglab/alpine-base-hl:arm64-$1"
-                docker push hackinglab/alpine-base-hl:arm64-$1
-                echo "=============================================="
-                echo "docker push hackinglab/alpine-base-hl:arm64-$1.0"
-                docker push hackinglab/alpine-base-hl:arm64-$1.0
+                #echo "=============================================="
+                #echo "docker push hackinglab/alpine-base-hl:arm64-latest"
+                #docker push hackinglab/alpine-base-hl:arm64-latest
+                #echo "=============================================="
+                #echo "docker push hackinglab/alpine-base-hl:arm64-$1"
+                #docker push hackinglab/alpine-base-hl:arm64-$1
+                #echo "=============================================="
+                #echo "docker push hackinglab/alpine-base-hl:arm64-$1.0"
+                #docker push hackinglab/alpine-base-hl:arm64-$1.0
 
 	;;
 
