d1e94960901daf8683bdbffeea6ddc6bbcb16fb7
Ivan Buetler <ivan.buetler@compass-security.com>
2022-11-08 20:51:05 +0100

    adding --push

--- build-release.sh
+++ build-release.sh
@@ -5,13 +5,13 @@ case `uname -m` in
 	x86_64)
 		echo "=============================================="
 		echo "building x86_64"
-		docker buildx build --platform linux/amd64 --no-cache -t hackinglab/alpine-base-hl:amd64-latest -t hackinglab/alpine-base-hl:amd64-$1 -t hackinglab/alpine-base-hl:amd64-$1.0 -f Dockerfile.amd64 .
+		docker buildx build --push --platform linux/amd64 --no-cache -t hackinglab/alpine-base-hl:amd64-latest -t hackinglab/alpine-base-hl:amd64-$1 -t hackinglab/alpine-base-hl:amd64-$1.0 -f Dockerfile.amd64 .
 	;;
 
 	aarch64)
                 echo "=============================================="
                 echo "building arm64"
-                docker buildx build --platform linux/arm64 --no-cache -t hackinglab/alpine-base-hl:arm64-latest -t hackinglab/alpine-base-hl:arm64-$1 -t hackinglab/alpine-base-hl:arm64-$1.0 -f Dockerfile.arm64 .
+                docker buildx build --push --platform linux/arm64 --no-cache -t hackinglab/alpine-base-hl:arm64-latest -t hackinglab/alpine-base-hl:arm64-$1 -t hackinglab/alpine-base-hl:arm64-$1.0 -f Dockerfile.arm64 .
 	;;
 
 	*)
