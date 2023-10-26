42999e791287413f5fd1804aa951519b0720f2bb
Ivan Buetler <ivan.buetler@compass-security.com>
2023-10-26 19:17:33 +0200

    update

--- build-multi-arch-without-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:latest . 
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1  . 
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1.0 .
--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -3,3 +3,4 @@
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:latest . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1  . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1.0 . --push
+
